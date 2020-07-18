7b2515bab74fe31fc0dd043f1f53406daa0128d2
ai7603 <1693065785@qq.com>
2020-07-18 15:24:02 +0800

    modify rtl and coe dir

---
+++ rtl/cpu.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/07/18 15:17:34
+// Design Name: 
+// Module Name: cpu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu(
+
+    );
+
+// Fetch
+
+// Decode
+
+// Execute
+
+// Memory
+
+// Write Back
+
+endmodule
