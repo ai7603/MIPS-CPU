5412f841182392391ca9b891fff42cc32aac881f
ai7603 <1693065785@qq.com>
2020-07-18 12:28:25 +0800

    add rtl and coe dir

---
+++ rtl/soc_lite_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/07/18 12:24:13
+// Design Name: 
+// Module Name: soc_lite_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module soc_lite_top(
+
+    );
+endmodule
